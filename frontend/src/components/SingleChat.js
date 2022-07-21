a1417087e636ca40963ecdebd291500b6b984bcc
Sanket Mudholkar <93979254+sanket0708@users.noreply.github.com>
2022-07-21 17:32:46 -0400

    Adding and removing users from group

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -1,9 +1,10 @@
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { Box, IconButton, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { getSender , getSenderFull } from "../config/ChatLogics";
+import { getSender, getSenderFull } from "../config/ChatLogics";
 import { ChatState } from "../Context/ChatProvider";
 import ProfileModal from "./miscellaneous/ProfileModal";
+import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const { user, selectedChat, setSelectedChat } = ChatState();
@@ -35,10 +36,26 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
             ) : (
               <>
                 {selectedChat.chatName.toUpperCase()}
-                {/* <UpdateGroupChatModal fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} /> */}
+                <UpdateGroupChatModal
+                  fetchAgain={fetchAgain}
+                  setFetchAgain={setFetchAgain}
+                />
               </>
             )}
           </Text>
+          <Box
+            display="flex"
+            flexDir="column"
+            justifyContent="flex-end"
+            p={3}
+            bg="E8E8E8"
+            w="100%"
+            h="100%"
+            borderRadius="lg"
+            overflowY="hidden"
+          >
+            {/* Message Here */}
+          </Box>
         </>
       ) : (
         <Box
